ccc0078caa260108928d06583c84fa515535e162
Ben Collins <bwcollins97@gmail.com>
2019-10-12 21:28:31 +1100

    Particle documentation and format changes

--- FARM-LIFE/particles/Particle.hpp
+++ FARM-LIFE/particles/Particle.hpp
@@ -4,32 +4,38 @@
 #include "glm/glm.hpp"
 #include <vector>
 
-namespace particle {
-
-  class Particle {
+namespace particle
+{
+	/**
+	 * Particle properties.
+	 * 
+	 * These properties dictate each particles shape, size, colour, location, 
+	 * travel speed and direction as well as perpendicular orientation to 
+	 * camera view.
+	 */
+	class Particle
+	{
     public:
+		typedef std::vector<Particle> ParticleBuffer;
 
-    typedef std::vector<Particle> ParticleBuffer;
+		glm::vec3 position;
+		glm::vec3 velocity;
+		glm::vec4 colour;
+		float fRotate;
+		float fSize;
+		float fAge;
+		float fLifeTime;
 
-    glm::vec3 position;
-    glm::vec3 velocity;
-    glm::vec4 colour;
-    float fRotate;
-    float fSize;
-    float fAge;
-    float fLifeTime;
-
-    Particle() {
-      position = glm::vec3(0);
-      velocity = glm::vec3(0);
-      colour = glm::vec4(0);
-      fRotate = 0;
-	  fSize = 0;
-      fAge = 0;
-      fLifeTime = 0;
-    }
-  };
-
+		Particle() {
+			position = glm::vec3(0);
+			velocity = glm::vec3(0);
+			colour = glm::vec4(0);
+			fRotate = 0;
+			fSize = 0;
+			fAge = 0;
+			fLifeTime = 0;
+		}
+	};
 }
 
-#endif  // ASSIGNMENT_PARTICLES_HPP
+#endif
