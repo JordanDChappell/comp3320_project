08d8b08ca73cece887cb4e2accd2e92e550e9c87
Ben Collins <bwcollins97@gmail.com>
2019-10-13 16:54:59 +1100

    Fix texture load error with particles

--- FARM-LIFE/particles/ParticleEffect.hpp
+++ FARM-LIFE/particles/ParticleEffect.hpp
@@ -113,7 +113,35 @@ namespace particle
 			glDeleteTextures(1, &textureID);
 		}
 
-		textureID = SOIL_load_OGL_texture(fileName.c_str(), SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+		glGenTextures(1, &textureID);
+
+		int width, height, nrComponents;
+		unsigned char* data = SOIL_load_image(fileName.c_str(), &width, &height, &nrComponents, 0);
+		if (data)
+		{
+			GLenum format = GL_RED;
+			if (nrComponents == 1) format = GL_RED;
+			else if (nrComponents == 3) format = GL_RGB;
+			else if (nrComponents == 4) format = GL_RGBA;
+
+			glBindTexture(GL_TEXTURE_2D, textureID);
+			glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			SOIL_free_image_data(data);
+		}
+		else
+		{
+			std::cout << "(SOIL) Texture failed to load at path: " << fileName << std::endl;
+			SOIL_free_image_data(data);
+		}
+
+		//textureID = SOIL_load_OGL_texture(fileName.c_str(), SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
 
 		return (textureID != 0);
 	}
